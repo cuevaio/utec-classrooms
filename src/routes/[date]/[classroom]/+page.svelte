9dc8dc717fb61952947ca26b30f7ec06a8d7b4c5
Anthony Cueva <hi.cuevantn@gmail.com>
2023-09-30 17:52:02 -0500

    hoursss

--- src/routes/[date]/[classroom]/+page.svelte
+++ src/routes/[date]/[classroom]/+page.svelte
@@ -3,6 +3,35 @@
 
 	/** @type {import('./$types').PageServerData} */
 	export let data;
+
+	/** @type {number[]}*/
+	let hours_to_hide = [];
+
+	$: {
+    hours_to_hide = [];
+		data.events.forEach((e) => {
+			if (!!e.end && !!e.start) {
+				// @ts-ignore
+				let long = Math.abs(e.end - e.start) / 36e5;
+
+				if (long > 1) {
+					for (let i = 1; i < long; i++) {
+						hours_to_hide = [
+							...hours_to_hide,
+							Number(
+								new Date(e.start.getTime() + i * 36e5).toLocaleTimeString('es-PE', {
+									hour: '2-digit',
+									hour12: false
+								})
+							)
+						];
+					}
+				}
+			}
+		});
+	}
+
+	$: console.log(hours_to_hide);
 </script>
 
 <svelte:head>
@@ -57,10 +86,7 @@
 		class="absolute top-0 bottom-0 right-0 left-0 grid gap-2 -z-5"
 	>
 		{#each new Array(15).fill(0) as _, i}
-			<div
-				class="relative data-[hide=true]:opacity-0"
-				data-hide={data.hours_to_hide.includes(i - 5)}
-			>
+			<div class="relative data-[hide=true]:opacity-0" data-hide={hours_to_hide.includes(i + 7)}>
 				<div
 					class="ml-2 absolute -top-3 border h-6 w-16 rounded-full text-gray-700 bg-white flex items-center justify-center text-sm z-10"
 				>
