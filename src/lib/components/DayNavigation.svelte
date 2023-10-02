27bb8d08dfb80168144f4eb4a5863d9a1dbc2d4c
Anthony Cueva <hi.cuevantn@gmail.com>
2023-10-02 13:24:51 -0500

    apartently will deploy 5

--- src/lib/components/DayNavigation.svelte
+++ src/lib/components/DayNavigation.svelte
@@ -14,13 +14,30 @@
 	let date_lima = new Date();
 	date_lima.setHours(date_lima.getHours() - 5);
 	let actual_today = date_lima.toISOString().split('T')[0];
+
+	// @ts-ignore
+	$: diffTimeNext = Math.abs(new Date(tomorrow) - new Date());
+	$: diffDaysNext = Math.ceil(diffTimeNext / (1000 * 60 * 60 * 24));
+
+	// @ts-ignore
+	$: diffTimePrev = Math.abs(new Date() - new Date(yesterday));
+	$: diffDaysPrev = Math.ceil(diffTimePrev / (1000 * 60 * 60 * 24));
 </script>
 
-<a
-	class="border w-32 h-8 rounded-lg flex items-center justify-center"
-	href={`/${yesterday}/${classroom || ''}`}>{actual_today === today ? 'ayer' : '-1 día'}</a
->
-<a
-	class="border w-32 h-8 rounded-lg flex items-center justify-center"
-	href={`/${tomorrow}/${classroom || ''}`}>{actual_today === today ? 'mañana' : '+1 día'}</a
->
+{#if diffDaysPrev < 10}
+	<a
+		class="border w-32 h-8 rounded-lg flex items-center justify-center"
+		href={`/${yesterday}/${classroom || ''}`}>{actual_today === today ? 'ayer' : '-1 día'}</a
+	>
+{:else}
+	<div class="w-32 h-8" />
+{/if}
+
+{#if diffDaysNext < 10}
+	<a
+		class="border w-32 h-8 rounded-lg flex items-center justify-center"
+		href={`/${tomorrow}/${classroom || ''}`}>{actual_today === today ? 'mañana' : '+1 día'}</a
+	>
+{:else}
+	<div class="w-32 h-8" />
+{/if}
